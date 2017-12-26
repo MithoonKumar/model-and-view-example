e6ebb9367f13aa3474aa8f9dad0da39f29cfd4c3
tikTokTik <mithoonk@flock.com>
2017-12-26 16:00:14 +0530

    Create Repo

---
+++ src/main/java/com/example/demomodelview/DemoModelViewApplication.java
@@ -0,0 +1,12 @@
+package com.example.demomodelview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoModelViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoModelViewApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demomodelview/controller/controller.java
@@ -0,0 +1,31 @@
+package com.example.demomodelview.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.WebParam;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class controller {
+    @RequestMapping("/new")
+    public String func(Model model){
+        model.addAttribute("one",1);
+        return "new";
+    }
+
+    @RequestMapping("/index")
+    public ModelAndView func1(){
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("one","one");
+        Map<String, Object> model1 = new HashMap<String, Object>();
+        model1.put("one","one hi hai bhai");
+        ModelAndView m = new ModelAndView("index","model",model);
+        return m;
+    }
+}
