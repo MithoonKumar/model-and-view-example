260d236954469e47346424df40a17b37fd0c8f84
tikTokTik <mithoonk@flock.com>
2018-01-08 16:44:41 +0530

    Use gson library to serialize and deserialize java into json and vice versa

--- src/main/java/com/example/demomodelview/controller/controller.java
+++ src/main/java/com/example/demomodelview/controller/controller.java
@@ -1,14 +1,16 @@
 package com.example.demomodelview.controller;
 
 
+import com.example.demomodelview.model.model1;
+import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.jws.WebParam;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -31,5 +33,23 @@ public class controller {
     @RequestMapping("/one")
     public String redirectMethod(){
         return "redirect:/new";
+    }
+    @RequestMapping("/gson")
+    @ResponseBody
+    public String useOfGson(){
+        model1 model = new model1();
+        model.setK(10);
+        model.setStr("Mithoon");
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(model);
+        return jsonString;
+    }
+
+    @RequestMapping(value="/post",method= RequestMethod.POST)
+    @ResponseBody
+    public String useOfGson1(@RequestBody String str){
+        Gson gson = new Gson();
+        model1 newModel = gson.fromJson(str,model1.class);
+        return "success";
     }
 }
---
+++ src/main/java/com/example/demomodelview/model/model1.java
@@ -0,0 +1,25 @@
+package com.example.demomodelview.model;
+
+public class model1 {
+    private int k;
+    private String str;
+
+    public model1() {
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
